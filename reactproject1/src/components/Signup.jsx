4c7c15f4e01dcfd8b5e82fa83cc756d9cee328b4
Vedang Sawant <sawantvedang2014@gmail.com>
2024-01-13 12:11:00 +0530

    commit 1 sign up frontend

--- reactproject1/src/components/Signup.jsx
+++ reactproject1/src/components/Signup.jsx
@@ -1,18 +1,46 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom';
+import axios from 'axios'; // Import axios
 import './styles/Login.css';
 
 const Signup = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [signupSuccess, setSignupSuccess] = useState(false);
 
-    const handleSignup = (e) => {
+    const handleSignup = async (e) => {
         e.preventDefault();
-        // Handle signup logic here
+
+        try {
+            // Make a POST request to your signup API
+            const response = await axios.post('http://localhost:1337/users/signup', {
+                username: email, // Assuming your API expects "username" instead of "email"
+                password: password,
+            });
+
+            console.log(response.data); // Log the response data
+            if(response.ok){
+                setSignupSuccess
+            }
+            // Handle success, e.g., redirect to login page
+        } catch (error) {
+            console.error('Signup failed:', error);
+
+            // Handle error, e.g., display an error message to the user
+        }
+    };
+    const SignupSuccessPage = () => {
+        return (
+            <div>
+                <h2>SignUp Successful</h2>
+            </div>
+        );
     };
 
     return (
         <div className="form-container">
+            {!signupSuccess ? (
+                <>
             <h2>Signup</h2>
             <form onSubmit={handleSignup} className="form">
                 <input
@@ -29,10 +57,17 @@ const Signup = () => {
                     onChange={(e) => setPassword(e.target.value)}
                     className="input"
                 />
-                <button type="submit" className="button">Signup</button>
+                <button type="submit" className="button">
+                    Signup
+                </button>
             </form>
-            {/* Link to the Login page */}
-            <p>Already have an account? <Link to="/">Login</Link></p>
+            <p>
+                Already have an account? <Link to="/">Login</Link>
+            </p>
+            </>
+            ):(
+                <SignupSuccessPage/>
+            )}
         </div>
     );
 };
